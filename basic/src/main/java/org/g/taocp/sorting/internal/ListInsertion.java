0d927474bd75c1b850a3225407e4964a8efced55
Gaven <gaven_yeung@163.com>
2021-10-26 15:26:32 +0800

    Add: debug info

--- basic/src/main/java/org/g/taocp/sorting/internal/ListInsertion.java
+++ basic/src/main/java/org/g/taocp/sorting/internal/ListInsertion.java
@@ -1,20 +1,31 @@
 package org.g.taocp.sorting.internal;
 
+import java.util.Arrays;
+
 public class ListInsertion {
     public static void main(String[] args) {
         // 稍做变化，改为与书中的下标 1...N 一致
         int[] R = {0, 503, 87, 512, 61, 908, 170, 897, 275, 653, 426, 154, 509, 612, 677, 765, 703};
         int N = R.length - 1;
 
+        System.out.println(Arrays.toString(R));
+
+        System.out.println("-----");
+
         int[] L = new int[N + 1];
 
         L[0] = N;
         L[N] = 0;
 
+        System.out.println("L：" + Arrays.toString(L));
+        print(R, L);
+
         for (int j = N - 1; j > 0; j--) {
             int p = L[0];
             int q = 0;
             int t = R[j];
+
+            System.out.println("t = " + t);
 
             while (R[p] < t && p > 0) {
                 q = p;
@@ -23,12 +34,24 @@ public class ListInsertion {
 
             L[q] = j;
             L[j] = p;
+
+            System.out.println("L：" + Arrays.toString(L));
+            print(R, L);
         }
+
+        System.out.println("-----");
+        print(R, L);
+    }
+
+    private static void print(int[] R, int[] L) {
+        System.out.printf("R：");
 
         int p = L[0];
         while (p > 0) {
             System.out.printf("%d, ", R[p]);
             p = L[p];
         }
+
+        System.out.println();
     }
 }
